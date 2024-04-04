0a6de6fd28ad467a4aef9db8e32e5fbc363c46c3
tranphuongVy <22521708@gm.uit.edu.vn>
2024-04-04 23:57:40 +0700

    SignUp

--- DEMO/GUI/SignUp.xaml.cs
+++ DEMO/GUI/SignUp.xaml.cs
@@ -22,8 +22,126 @@ namespace GUI
         public SignUp()
         {
             InitializeComponent();
+            //Day
+            List<int> days = new List<int>();
+            for (int i = 1; i <= 31; i++)
+            {
+                days.Add(i);
+            }
+
+            // Thiết lập dữ liệu cho ComboBox
+            comboBox.ItemsSource = days;
+            //Month
+            List<int> months = new List<int>();
+            for (int i = 1; i <= 12; i++)
+            {
+                months.Add(i);
+            }
+
+            // Thiết lập dữ liệu cho ComboBox
+            comboBox1.ItemsSource = months;
+            //Year
+            // Lấy năm hiện tại
+            int currentYear = DateTime.Now.Year;
+
+            // Khởi tạo danh sách các năm
+            List<int> years = new List<int>();
+            for (int i = currentYear - 120; i <= currentYear + 50; i++) // Lấy 10 năm trước và sau năm hiện tại
+            {
+                years.Add(i);
+            }
+
+            // Thiết lập dữ liệu cho ComboBox
+            comboBox2.ItemsSource = years;
         }
 
+        private void textFName_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtFName.Focus();
+        }
 
+        private void txtFName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFName.Text) && txtFName.Text.Length > 0)
+            {
+                textFName.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textFName.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textLName_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtLName.Focus();
+        }
+
+        private void txtLName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtLName.Text) && txtLName.Text.Length > 0)
+            {
+                textLName.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textLName.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textMailAdd_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtMailAdd.Focus();
+        }
+
+        private void txtMailAdd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtMailAdd.Text) && txtMailAdd.Text.Length > 0)
+            {
+                textMailAdd.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textMailAdd.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textRePassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtRePassword.Focus();
+        }
+
+        private void txtRePassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtRePassword.Password) && txtRePassword.Password.Length > 0)
+            {
+                textRePassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textRePassword.Visibility = Visibility.Visible;
+            }
+        }
+
+  
+
+        
     }
 }
