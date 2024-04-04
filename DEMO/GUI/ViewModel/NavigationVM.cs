d4430151b542e03f33e18f72fa7295c14d9545c8
tranphuongVy <22521708@gm.uit.edu.vn>
2024-04-04 22:05:05 +0700

    GUI

---
+++ DEMO/GUI/ViewModel/NavigationVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GUI.Ultilities; 
+
+namespace GUI.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand Window1Command { get; set; }
+        public ICommand Window2Command { get; set; }
+        public ICommand Window3Command { get; set; }
+        public ICommand Window4Command { get; set; }
+        public ICommand Window5Command { get; set; }
+
+        private void Window1(object obj) => CurrentView = new Window1VM();
+        private void Window2(object obj) => CurrentView = new Window2VM();
+        private void Window3(object obj) => CurrentView = new Window3VM();
+        private void Window4(object obj) => CurrentView = new Window4VM();
+        private void Window5(object obj) => CurrentView = new Window5VM();
+
+        public NavigationVM()
+        {
+            Window1Command = new RelayCommand(Window1);
+            Window2Command = new RelayCommand(Window2);
+            Window3Command = new RelayCommand(Window3);
+            Window4Command = new RelayCommand(Window4);
+            Window5Command = new RelayCommand(Window5);
+
+            CurrentView = new Window1VM();
+        }
+    }
+}
