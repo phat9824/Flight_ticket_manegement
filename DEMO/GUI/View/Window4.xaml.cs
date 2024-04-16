435bb8b19c9dd02eaefe27da490aa66273e5a6ce
Azel <thaingocquan235@gmail.com>
2024-04-17 01:06:35 +0700

    update

--- DEMO/GUI/View/Window4.xaml.cs
+++ DEMO/GUI/View/Window4.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,40 @@ namespace GUI.View
     /// </summary>
     public partial class Window4 : UserControl
     {
+        private ICollectionView collectionView;
+
         public Window4()
         {
             InitializeComponent();
+
+            ObservableCollection<TicketClass> ticketList = new ObservableCollection<TicketClass>
+            {
+                new TicketClass { ID = "1", Name = "Standard", Quantity = 70 },
+                new TicketClass { ID = "2", Name = "Premium1", Quantity = 20 },
+                new TicketClass { ID = "3", Name = "Premium2", Quantity = 20 },
+                new TicketClass { ID = "4", Name = "VIP1", Quantity = 10 },
+                new TicketClass { ID = "5", Name = "VIP2", Quantity = 5 },
+                new TicketClass { ID = "6", Name = "Elon Musk", Quantity = 1 }
+
+            };
+            collectionView = CollectionViewSource.GetDefaultView(ticketList);
+            dataGrid.ItemsSource = collectionView;
         }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            TicketClass selectedTicket = (TicketClass)dataGrid.SelectedItem;
+            if (selectedTicket != null)
+            {
+                ((ObservableCollection<TicketClass>)collectionView.SourceCollection).Remove(selectedTicket);
+                dataGrid.ItemsSource = collectionView;
+            }
+        }
+    }
+
+    public class TicketClass
+    {
+        public string ID { get; set; }
+        public string Name { get; set;}
+        public int Quantity { get; set; }
     }
 }
