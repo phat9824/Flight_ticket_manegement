2a039c8ad19676fba6d42cc4dc1ba9f119495464
tranphuongVy <22521708@gm.uit.edu.vn>
2024-04-27 23:40:21 +0700

    update

---
+++ DEMO/GUI/ViewModel/Window10VM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GUI.Model;
+
+namespace GUI.ViewModel
+{
+    class Window10VM : Ultilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+
+        public string Window10
+        {
+            get { return _pageModel.Window10; }
+            set { _pageModel.Window10 = value; OnPropertyChanged(); }
+        }
+
+        public Window10VM()
+        {
+            _pageModel = new PageModel();
+        }
+    }
+}
