7a06db2059d26fad05f2a30972eb92d11b5c5c8a
phat9824 <nguyenhungphat09082004@gmail.com>
2024-06-06 21:09:42 +0700

    fix name varible in parameter

--- DEMO/DAL/ParameterAccess.cs
+++ DEMO/DAL/ParameterAccess.cs
@@ -22,7 +22,7 @@ namespace DAL
             try
             {
                 con.Open();
-                string query = @"SELECT AirportCount, DepartureTime, IntermediateAirportCount, MinStopTime, MaxStopTime, TicketClassCount, SlowestBookingTime, CancelTime
+                string query = @"SELECT AirportCount, MinFlighTime, IntermediateAirportCount, MinStopTime, MaxStopTime, TicketClassCount, SlowestBookingTime, CancelTime
                              FROM PARAMETER
                              where isDeleted = 0";
 
@@ -36,7 +36,7 @@ namespace DAL
                             parameter = new ParameterDTO()
                             {
                                 AirportCount = Convert.ToInt32(reader["AirportCount"]),
-                                DepartureTime = (TimeSpan)reader["DepartureTime"],
+                                MinFlighTime = (TimeSpan)reader["MinFlighTime"],
                                 IntermediateAirportCount = Convert.ToInt32(reader["IntermediateAirportCount"]),
                                 MinStopTime = (TimeSpan)reader["MinStopTime"],
                                 MaxStopTime = (TimeSpan)reader["MaxStopTime"],
@@ -81,78 +81,8 @@ namespace DAL
         }
         public string InsertParamater(ParameterDTO parameter)
         {
-            DeleteParamater();
-            SqlConnection con = SqlConnectionData.Connect();
-            con.Open();
-            using (SqlTransaction transaction = con.BeginTransaction())
-            {
-                try
-                {
-                    using (SqlCommand cmd = new SqlCommand())
-                    {
-                        cmd.CommandType = CommandType.Text;
-                        cmd.CommandText = "INSERT INTO PARAMETER (AirportCount, DepartureTime, IntermediateAirportCount, MinStopTime, MaxStopTime, TicketClassCount, TicketClassCount, SlowestBookingTime, CancelTime, isDeleted)" +
-                                        " VALUES (@AirportCount, @DepartureTime, @IntermediateAirportCount, @MinStopTime, @MaxStopTime, @TicketClassCount, @SlowestBookingTime, @CancelTime, 0);";
-                        cmd.Connection = con;
-                        cmd.Transaction = transaction;
 
-                        SqlParameter parAirportCount = new SqlParameter("@AirportCount", SqlDbType.Int)
-                        {
-                            Value = parameter.AirportCount
-                        };
-                        SqlParameter parDepartureTime = new SqlParameter("@DepartureTime", SqlDbType.Time)
-                        {
-                            Value = parameter.DepartureTime
-                        };
-                        SqlParameter parIntermediateAirportCount = new SqlParameter("@IntermediateAirportCount", SqlDbType.Int)
-                        {
-                            Value = parameter.IntermediateAirportCount
-                        };
-                        SqlParameter parMinStopTime = new SqlParameter("@MinStopTime", SqlDbType.Time)
-                        {
-                            Value = parameter.MinStopTime
-                        };
-                        SqlParameter parMaxStopTime = new SqlParameter("@MaxStopTime", SqlDbType.Time)
-                        {
-                            Value = parameter.MaxStopTime
-                        };
-                        SqlParameter parTicketClassCount = new SqlParameter("@TicketClassCount", SqlDbType.Int)
-                        {
-                            Value = parameter.TicketClassCount
-                        };
-                        SqlParameter parSlowestBookingTime = new SqlParameter("@SlowestBookingTime", SqlDbType.Time)
-                        {
-                            Value = parameter.SlowestBookingTime
-                        };
-                        SqlParameter parCancelTime = new SqlParameter("@CancelTime", SqlDbType.Time)
-                        {
-                            Value = parameter.CancelTime
-                        };
-
-                        cmd.Parameters.Add(parAirportCount);
-                        cmd.Parameters.Add(parDepartureTime);
-                        cmd.Parameters.Add(parIntermediateAirportCount);
-                        cmd.Parameters.Add(parMaxStopTime);
-                        cmd.Parameters.Add(parTicketClassCount);
-                        cmd.Parameters.Add(parSlowestBookingTime);
-                        cmd.Parameters.Add(parCancelTime);
-
-                        int rowsAffected = cmd.ExecuteNonQuery();
-                        if (rowsAffected == 0)
-                        {
-                            transaction.Rollback();
-                            return "No rows were inserted.";
-                        }
-                    }
-                    transaction.Commit();
-                    return string.Empty; // Chuỗi rỗng xem như thành công
-                }
-                catch (Exception ex)
-                {
-                    transaction.Rollback();
-                    return $"Insert failed: {ex.Message}";
-                }
-            }
+            return "";
         }
     }
 }
